1aa4ccae025469e0bf44451fa4bf2091550b2ea5
Marcelo Brito <marcelobrito@marcelobrito.net>
2016-03-13 23:41:44 -0300

    Popup com descritivo dos bolos e Menu de Doces. Meta tags facebook. Imagem na tag h1

--- app/js/script.js
+++ app/js/script.js
@@ -27,10 +27,21 @@ $(document).ready(function () {
         ajax: {
             tError: '<a href="%url%">O conteúdo</a> não pôde ser carregado.'
         }
-    }
-
+    };
     $('.lista-interna').magnificPopup(magnificPopupSettings);
 
     magnificPopupSettings.type = 'iframe';
-    $('.contato-page .mapa').magnificPopup(magnificPopupSettings)
+    $('.contato-page .mapa').magnificPopup(magnificPopupSettings);
+
+    $('.popup').magnificPopup({
+        type: 'ajax',
+        ajax: {
+            settings: null, // Ajax settings object that will extend default one - http://api.jquery.com/jQuery.ajax/#jQuery-ajax-settings
+            // For example:
+            // settings: {cache:false, async:false}
+
+            cursor: 'mfp-ajax-cur', // CSS class that will be added to body during the loading (adds "progress" cursor)
+            tError: '<a href="%url%">The content</a> could not be loaded.' //  Error message, can contain %curr% and %total% tags if gallery is enabled
+        }
+    });
 });
